884344d053b5836a974a24204bd9cb28ab77fe46
Erdogan Taskesen <erdogant@gmail.com>
2020-09-26 21:44:17 +0200

    new version

--- d3heatmap/__init__.py
+++ d3heatmap/__init__.py
@@ -7,7 +7,7 @@ from d3heatmap.utils.adjmat_vec import (
 
 __author__ = 'Erdogan Tasksen'
 __email__ = 'erdogant@gmail.com'
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 
 # module level doc-string
 __doc__ = """
